cc2e8ff9e9950e78d57924c7728768c129b14f90
Ziyang Guo <guoziyang0033@gmail.com>
2019-10-17 19:54:16 +0800

    Lab1 cout问题

--- CO_Lab1/multiplex.v
+++ CO_Lab1/multiplex.v
@@ -59,16 +59,6 @@ module multiplex1_16(in, out, card);
             5'b00011: out = in[12];
             5'b00100: out = in[11];
             5'b00101: out = in[10];
-            5'b00110: out = in[9];
-            5'b00111: out = in[8];
-            5'b01000: out = in[7];
-            5'b01001: out = in[6];
-            5'b01010: out = in[5];
-            5'b01011: out = in[4];
-            5'b01100: out = in[3];
-            5'b01101: out = in[2];
-            5'b01110: out = in[1];
-            5'b01111: out = in[0];
         endcase
     end
 
